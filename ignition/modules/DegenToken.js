f5fddc14b5acb05c60d1913ada7e38510accd8e8
Rishav Kumar <mrsonic622@gmail.com>
2024-09-07 00:28:53 +0530

    Initial commit

--- ignition/modules/DegenToken.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("DegenGamingTokenModule", (m) => {
-  const degenGamingToken = m.contract("DegenToken", []);
-
-  return { degenGamingToken };
-});
